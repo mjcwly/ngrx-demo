8387f064d54dbf75a1ba18ad0e7a05ad1263bd1e
Matthew Cawley <matthew.cawley@systopia.co.uk>
2020-01-02 23:06:13 +0000

    Working implementation of the State Pattern

--- src/app/store/reducers/clock-setup.reducer.ts
+++ src/app/store/reducers/clock-setup.reducer.ts
@@ -2,31 +2,188 @@ import { Action, createReducer, on } from "@ngrx/store";
 import * as ClockSetupActions from '../actions/clock-setup.actions';
 
 export interface ClockSetupState {
-  day: number,
-  month: number,
-  year: number,
-  hours: number,
-  minutes: number,
-  mode: number,
-  instructions: string
+  dateTimeSetup: DateTimeSetup
 }
 
-const modeInstructions = {
-  0: "Please set the Year",
-  1: "Please set the Month",
-  2: "Please set the Day",
-  3: "Please set the Hours",
-  4: "Please set the Minutes",
+export class DateTimeSetup {
+  private yearState: DateTimeSetupState;
+  private monthState: DateTimeSetupState;
+  private dayState: DateTimeSetupState;
+  private hourState: DateTimeSetupState;
+  private minuteState: DateTimeSetupState;
+  private currentState: DateTimeSetupState;
+
+  constructor() {
+    console.log("DateTimeSetup Constructor");
+    this.yearState = new YearSetupState(this);
+    this.monthState = new MonthSetupState(this);
+    this.dayState = new DaySetupState(this);
+    this.hourState = new HourSetupState(this);
+    this.minuteState = new MinuteSetupState(this);
+    this.currentState = this.yearState;
+  }
+
+  previousValue = () => this.currentState.previousValue();
+
+  selectValue = () => {
+    this.currentState.selectValue();
+  }
+
+  nextValue = () => this.currentState.nextValue();
+  setCurrentState = (state: DateTimeSetupState) => this.currentState = state;
+  getYearState = () => this.yearState;
+  getMonthState = () => this.monthState;
+  getDayState = () => this.dayState;
+  getHourState = () => this.hourState;
+  getMinuteState = () => this.minuteState;
+  getCurrentState = () => this.currentState;
+}
+
+export interface DateTimeSetupState {
+  instructions: string;
+  selectedValue: number;
+  previousValue(): void;
+  selectValue(): void;
+  nextValue(): void;
+}
+
+export class YearSetupState implements DateTimeSetupState {
+
+  instructions: string;
+  selectedValue: number;
+  dateTimeSetup: DateTimeSetup;
+
+  previousValue(): void {
+    this.selectedValue--;
+  }
+
+  selectValue(): void { 
+    console.log("Year set to " + this.selectedValue);
+    this.dateTimeSetup.setCurrentState(this.dateTimeSetup.getMonthState());
+  }
+
+  nextValue(): void {
+    this.selectedValue++;
+  }
+
+  constructor(dateTimeSetup: DateTimeSetup) {
+    console.log("YearSetupState Constructor");
+    this.dateTimeSetup = dateTimeSetup;
+    this.selectedValue = 2020;
+    this.instructions = "Set the Year...";
+  }
+}
+
+export class MonthSetupState implements DateTimeSetupState {
+
+  instructions: string;
+  selectedValue: number;
+  dateTimeSetup: DateTimeSetup;
+
+  previousValue(): void {
+    this.selectedValue--;
+  }
+
+  selectValue(): void {
+    console.log("Month set to " + this.selectedValue);
+    this.dateTimeSetup.setCurrentState(this.dateTimeSetup.getDayState());
+  }
+
+  nextValue(): void {
+    this.selectedValue++;
+  }
+
+  constructor(dateTimeSetup: DateTimeSetup) {
+    console.log("MonthSetupState Constructor");
+    this.dateTimeSetup = dateTimeSetup;
+    this.selectedValue = 1;
+    this.instructions = "Set the Month...";
+  }
+}
+
+export class DaySetupState implements DateTimeSetupState {
+
+  instructions: string;
+  selectedValue: number;
+  dateTimeSetup: DateTimeSetup;
+
+  previousValue(): void {
+    this.selectedValue--;
+  }
+
+  selectValue(): void {
+    console.log("Day set to " + this.selectedValue);
+    this.dateTimeSetup.setCurrentState(this.dateTimeSetup.getHourState());
+  }
+
+  nextValue(): void {
+    this.selectedValue++;
+  }
+
+  constructor(dateTimeSetup: DateTimeSetup) {
+    console.log("DaySetupState Constructor");
+    this.dateTimeSetup = dateTimeSetup;
+    this.selectedValue = 10;
+    this.instructions = "Set the Day...";
+  }
+}
+
+export class HourSetupState implements DateTimeSetupState {
+
+  instructions: string;
+  selectedValue: number;
+  dateTimeSetup: DateTimeSetup;
+
+  previousValue(): void {
+    this.selectedValue--;
+  }
+
+  selectValue(): void {
+    console.log("Hours set to " + this.selectedValue);
+    this.dateTimeSetup.setCurrentState(this.dateTimeSetup.getMinuteState());
+  }
+
+  nextValue(): void {
+    this.selectedValue++;
+  }
+
+  constructor(dateTimeSetup: DateTimeSetup) {
+    console.log("HourSetupState Constructor");
+    this.dateTimeSetup = dateTimeSetup;
+    this.selectedValue = 12;
+    this.instructions = "Set the Hours...";
+  }
+}
+
+export class MinuteSetupState implements DateTimeSetupState {
+
+  instructions: string;
+  selectedValue: number;
+  dateTimeSetup: DateTimeSetup;
+
+  previousValue(): void {
+    this.selectedValue--;
+  }
+
+  selectValue(): void {
+    console.log("Minutes set to " + this.selectedValue);
+    this.dateTimeSetup.setCurrentState(this.dateTimeSetup.getYearState());
+  }
+
+  nextValue(): void {
+    this.selectedValue++;
+  }
+
+  constructor(dateTimeSetup: DateTimeSetup) {
+    console.log("MinuteSetupState Constructor");
+    this.dateTimeSetup = dateTimeSetup;
+    this.selectedValue = 30;
+    this.instructions = "Set the Minutes...";
+  }
 }
 
 export const initialState: ClockSetupState = {
-  day: new Date().getDate(),
-  month: new Date().getMonth() + 1,
-  year: new Date().getFullYear(),
-  hours: new Date().getHours(),
-  minutes: new Date().getMinutes(),
-  mode: 0,
-  instructions: modeInstructions[0]
+  dateTimeSetup: new DateTimeSetup()
 };
 
 const clockSetupReducer = createReducer(
@@ -34,32 +191,19 @@ const clockSetupReducer = createReducer(
 
   on(ClockSetupActions.previousValue, state => {
     let newState = { ...state };
-    if (state.mode === 0) newState.year--;
-    if (state.mode === 1) newState.month--;
-    if (state.mode === 2) newState.day--;
-    if (state.mode === 3) newState.hours--;
-    if (state.mode === 4) newState.minutes--;
+    newState.dateTimeSetup.previousValue();
     return newState
   }),
 
   on(ClockSetupActions.selectValue, state => {
     let newState = { ...state };
-    newState.mode++;
-    newState.instructions = modeInstructions[newState.mode];
-    if (newState.mode === 5) {
-      newState.mode = 0;
-      newState.instructions = modeInstructions[0];
-    }
+    newState.dateTimeSetup.selectValue();
     return newState
   }),
 
   on(ClockSetupActions.nextValue, state => {
     let newState = { ...state };
-    if (state.mode === 0) newState.year++;
-    if (state.mode === 1) newState.month++;
-    if (state.mode === 2) newState.day++;
-    if (state.mode === 3) newState.hours++;
-    if (state.mode === 4) newState.minutes++;
+    newState.dateTimeSetup.nextValue();
     return newState
   }),
 
--- src/app/store/services/clock-setup.selectors.ts
+++ src/app/store/services/clock-setup.selectors.ts
@@ -4,25 +4,23 @@ import { Injectable } from "@angular/core";
 
 const getEntityState = createFeatureSelector<EntityState>('entityCache');
 
+const getYear = createSelector(getEntityState, (state: EntityState) => state.clockSetup.dateTimeSetup.getYearState().selectedValue);
+const getMonth = createSelector(getEntityState, (state: EntityState) => state.clockSetup.dateTimeSetup.getMonthState().selectedValue);
+const getDay = createSelector(getEntityState, (state: EntityState) => state.clockSetup.dateTimeSetup.getDayState().selectedValue);
+const getHours = createSelector(getEntityState, (state: EntityState) => state.clockSetup.dateTimeSetup.getHourState().selectedValue);
+const getMinutes = createSelector(getEntityState, (state: EntityState) => state.clockSetup.dateTimeSetup.getMinuteState().selectedValue);
+const getInstructions = createSelector(getEntityState, (state: EntityState) => state.clockSetup.dateTimeSetup.getCurrentState().instructions);
+
 const getDateTime = createSelector(
-  getEntityState,
-  (state: EntityState) => {
-    const year = state.clockSetup.year;
-    const month = ('0' + state.clockSetup.month).slice(-2);
-    const day = ('0' + state.clockSetup.day).slice(-2);
-    const hours = ('0' + state.clockSetup.hours).slice(-2);
-    const minutes = ('0' + state.clockSetup.minutes).slice(-2);
-    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  getYear, getMonth, getDay, getHours, getMinutes,
+  (year, month, day, hours, minutes) => {
+    const MM = ('0' + month).slice(-2);
+    const dd = ('0' + day).slice(-2);
+    const hh = ('0' + hours).slice(-2);
+    const mm = ('0' + minutes).slice(-2);
+    return `${year}-${MM}-${dd} ${hh}:${mm}`;
   }
 );
-
-const getYear = createSelector(getEntityState, (state: EntityState) => state.clockSetup.year);
-const getMonth = createSelector(getEntityState, (state: EntityState) => state.clockSetup.month);
-const getDay = createSelector(getEntityState, (state: EntityState) => state.clockSetup.day);
-const getHours = createSelector(getEntityState, (state: EntityState) => state.clockSetup.hours);
-const getMinutes = createSelector(getEntityState, (state: EntityState) => state.clockSetup.minutes);
-const getMode = createSelector(getEntityState, (state: EntityState) => state.clockSetup.mode);
-const getInstructions = createSelector(getEntityState, (state: EntityState) => state.clockSetup.instructions);
 
 @Injectable()
 export class ClockSetupSelectors {
@@ -33,6 +31,5 @@ export class ClockSetupSelectors {
   day$ = this.store.select(getDay);
   hours$ = this.store.select(getHours);
   minutes$ = this.store.select(getMinutes);
-  mode$ = this.store.select(getMode);
   instructions$ = this.store.select(getInstructions);
 }

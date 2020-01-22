c3cb77e6048107b56858019b8b29808ba9e19a23
Matthew Cawley <matthew.cawley@systopia.co.uk>
2020-01-22 23:56:01 +0000

    Better date time display

--- src/app/clock-setup/clock-setup.component.ts
+++ src/app/clock-setup/clock-setup.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ClockSetupDispatchers } from '../store/services/clock-setup.dispatchers';
 import { ClockSetupSelectors } from '../store/services/clock-setup.selectors';
 import { Observable } from 'rxjs';
+import { ClockSetupMode } from '../store/reducers/clock-setup.reducer';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-clock-setup',
@@ -9,14 +11,26 @@ import { Observable } from 'rxjs';
   styleUrls: ['./clock-setup.component.scss']
 })
 export class ClockSetupComponent implements OnInit {
-  
+
   dateTime$: Observable<string>;
+  mode$: Observable<ClockSetupMode>;
+  year$: Observable<number>;
+  month$: Observable<string>;
+  day$: Observable<string>;
+  hours$: Observable<number>;
+  minutes$: Observable<number>;
 
   constructor(
     private clockSetupDispatchers: ClockSetupDispatchers,
     private clockSetupSelectors: ClockSetupSelectors
-  ) { 
+  ) {
     this.dateTime$ = this.clockSetupSelectors.dateTime$;
+    this.year$ = this.clockSetupSelectors.year$;
+    this.month$ = this.clockSetupSelectors.month$;
+    this.day$ = this.clockSetupSelectors.day$;
+    this.hours$ = this.clockSetupSelectors.hours$;
+    this.minutes$ = this.clockSetupSelectors.minutes$;
+    this.mode$ = this.clockSetupSelectors.mode$;
   }
 
   ngOnInit() { }
@@ -25,12 +39,12 @@ export class ClockSetupComponent implements OnInit {
     console.log("Left Button Clicked");
     this.clockSetupDispatchers.previousValue();
   }
-  
+
   onSetButtonClicked() {
     console.log("Set Button Clicked");
     this.clockSetupDispatchers.selectValue();
   }
-  
+
   onRightButtonClicked() {
     console.log("Right Button Clicked");
     this.clockSetupDispatchers.nextValue();
--- src/app/store/services/clock-setup.selectors.ts
+++ src/app/store/services/clock-setup.selectors.ts
@@ -17,15 +17,30 @@ const getDateTime = createSelector(
 );
 
 const getYear = createSelector(getEntityState, (state: EntityState) => state.clockSetup.year);
-const getMonth = createSelector(getEntityState, (state: EntityState) => state.clockSetup.month);
-const getDay = createSelector(getEntityState, (state: EntityState) => state.clockSetup.day);
+
+const getMonth = createSelector(
+  getEntityState,
+  (state: EntityState) => {
+    const months = { 1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec' };
+    return months[state.clockSetup.month]
+  });
+
+const getDay = createSelector(
+  getEntityState,
+  (state: EntityState) => {
+    let d = state.clockSetup.day;
+    let result = '' + d + (31 == d || 21 == d || 1 == d ? "st" : 22 == d || 2 == d ? "nd" : 23 == d || 3 == d ? "rd" : "th")
+    return result;
+  }
+)
+
 const getHours = createSelector(getEntityState, (state: EntityState) => state.clockSetup.hours);
 const getMinutes = createSelector(getEntityState, (state: EntityState) => state.clockSetup.minutes);
 const getMode = createSelector(getEntityState, (state: EntityState) => state.clockSetup.mode);
 
 @Injectable()
 export class ClockSetupSelectors {
-  constructor(private store: Store<EntityState>) {}
+  constructor(private store: Store<EntityState>) { }
   dateTime$ = this.store.select(getDateTime);
   year$ = this.store.select(getYear);
   month$ = this.store.select(getMonth);
